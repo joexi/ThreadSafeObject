e6fb0b817e6fb26373c1bf953b08ac1922622c2b
JoeXi <407154812@qq.com>
2013-05-09 15:05:15 +0800

    Initial Commit

---
+++ JXMultiThreadObject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JXMultiThreadObject
+//
+//  Created by Joe on 13-5-9.
+//  Copyright (c) 2013年 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
